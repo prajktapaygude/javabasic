0fc047ed8af80f6fda7c354f8fc4bd087c09ae00
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2026-06-22 17:45:18 +0530

    Add files via upload

---
+++ BrackStatement.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class BrackStatement {
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 1; i <= 50; i++) {
+			if(i==18)
+				break;
+			else
+			System.out.print(i+" ");
+		}
+
+	System.out.println("\n\nOut of loop");		
+
+}
+}
